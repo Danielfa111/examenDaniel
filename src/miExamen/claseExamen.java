a014d4bd7c2582ecad6c6e9c28de320129e2161d
Danielfa111 <danielfa11@educastur.es>
2024-02-23 19:01:57 +0100

    Añadidos los primeros ficheros al proyecto.

---
+++ src/miExamen/claseExamen.java
@@ -0,0 +1,13 @@
+
+package miExamen;
+
+/**
+ *
+ * @author Daniel Fernández Álvarez
+ */
+public class claseExamen {
+    
+    public static void main(String[] args) {
+        System.out.println("Estoy haciendo el examen de la UT3");
+    }
+}
