eef82c671ab14cc65335749459167d2bc78d7b2e
Danielfa111 <danielfa11@educastur.es>
2024-02-23 19:24:40 +0100

    Añadidos comentarios de documentación en claseExamen.

--- src/miExamen/claseExamen.java
+++ src/miExamen/claseExamen.java
@@ -2,8 +2,12 @@
 package miExamen;
 
 /**
- *
+ * Entornos de desarrollo UT3
+ * Esta clase sirve para realizar el examen de la UT3 del módulo Entornos de Desarrollo
+ * @see Curso de Git y github de píldoras informática https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU
  * @author Daniel Fernández Álvarez
+ * @version 1.0.0
+ * @since 23/02/2024
  */
 public class claseExamen {
     
